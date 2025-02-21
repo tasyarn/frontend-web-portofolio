3d8e6ccdd571db0ecfe55cf9c38ac6989e6d49f9
tasyarn <101511354+tasyarn@users.noreply.github.com>
2025-02-21 23:14:25 +0700

    homepage

---
+++ src/components/MetaSeo.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function MetaSeo({ title, description }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,44 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+import { Mansalva } from "next/font/google";
+
+const mansalva = Mansalva({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#E4E0E1] min-h-screen flex flex-col mb-10">
       <Navbar />
-      <main className="flex flex-col items-center justify-center h-screen text-center">
-        <h1 className="text-4xl font-bold text-gray-900">Welcome to My Portfolio</h1>
-        <p className="text-gray-600 mt-4">Explore my projects and experiences.</p>
+      <main className="flex flex-col items-center justify-center flex-1 text-center">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="flex flex-col items-center mt-4"
+        >
+          <Image
+            src="/images/profile.png"
+            height={200}
+            width={200}
+            alt="Profile Image"
+            className="hover:scale-105 transition-transform duration-300"
+          />
+          <h1 className={`${mansalva.className} text-4xl text-gray-900`}>
+            <Typewriter
+              options={{
+                strings: ["Welcome to My Portfolio Website", "I'm Tasya Rania Arinastia", "Contact Me, ASAP!"],
+                autoStart: true,
+                loop: true,
+                delay: 75,
+              }}
+            />
+          </h1>
+          <p className="text-gray-600 mt-4">
+            Explore my projects and experiences.
+          </p>
+        </motion.div>
       </main>
       <Footer />
     </div>
